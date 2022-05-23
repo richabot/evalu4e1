371f6b0bb44ebe659c6f1620afe3ebc578caf80b
richa shah <lavaformama@gmail.com>
2022-05-23 16:33:35 +0530

    richa

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,19 @@
+
+
 import React from "react";
 import styles from "./addTask.module.css";
 
-const AddTask = () => {
+const AddTask = ({data,setData,value,setValue}) => {
   // NOTE: do not delete `data-cy` key value pair
+const reset=()=>{
+  setData("");
+}
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
+      <input data-cy="add-task-input" className={styles.input1} value={value}type="text" placeholder="Add task..." onChange={(e)=>{setValue(e.target.value)} } />
+      <button data-cy="add-task-button"  disabled={!value} onChange={reset} className={styles.addbtn}  onClick={()=>{setData([...data,{id:Date.now(),text:value,done:false,count:1}],setValue(""))}} 
+    
+     >+</button>
     </div>
   );
 };
